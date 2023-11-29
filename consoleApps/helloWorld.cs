c04cc4302f5980020a2a3a95ecc469133119e5fe
Paul Ambrose <pablo_rose34@hotmail.com>
2023-11-29 10:00:23 +0000

    Create helloWorld.cs

---
+++ consoleApps/helloWorld.cs
@@ -0,0 +1,11 @@
+//https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-8-0
+
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
