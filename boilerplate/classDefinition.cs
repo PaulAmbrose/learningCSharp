c6176b6b53dc0430090543bc453ab787b0e99315
Paul Ambrose <pablo_rose34@hotmail.com>
2023-11-29 10:14:16 +0000

    Update and rename text.txt to classDefinition.cs

---
+++ boilerplate/classDefinition.cs
@@ -0,0 +1,9 @@
+//Class definition
+//[access modifier] - [class] - [identifier]
+public class Customer
+{
+   // Fields, properties, methods and events go here...
+}
+
+//Creating an object
+Customer object1 = new Customer();
