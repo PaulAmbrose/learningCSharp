1212c132a078c10c34959519dee86d106b0378a3
Paul Ambrose <pablo_rose34@hotmail.com>
2023-12-18 08:57:39 +0000

    Add files via upload

---
+++ consoleApps/pantryAppCSharp/classes/Kitchen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pantryAppCSharp
+{
+    class Kitchen
+    {
+        public string Name;
+        public string[] Locations = new string[4];
+        public int TransferFailure;
+        public int TransferSuccess;
+
+
+    }
+}
