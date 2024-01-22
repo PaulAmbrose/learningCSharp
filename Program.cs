3c84ea4cb810b6a44131b18d9381572ca3602864
Paul Ambrose <pablo_rose34@hotmail.com>
2024-01-22 20:49:16 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pantryAppCSharp
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+
+            /*Test1 - Able to create a kitchen, complete with places for food to be stored:
+            fridge, freezer, store cupboards, fruit bowl*/
+            
+            Kitchen mykitchen = new Kitchen();
+                //Console.WriteLine(mykitchen.Name);
+            //mykitchen.fridge.Add("A testItem");
+                //Console.WriteLine("This is the fridge, in which I have stored " + mykitchen.fridge[0]);
+
+            //Test3
+            mykitchen.AddItems(mykitchen);
+
+
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
