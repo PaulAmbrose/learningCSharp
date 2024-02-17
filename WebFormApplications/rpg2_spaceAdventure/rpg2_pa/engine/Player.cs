bf98250336965062a380d49e3d711cde7581b38f
Paul Ambrose <pablo_rose34@hotmail.com>
2024-02-17 10:29:04 +0000

    Add files via upload

---
+++ WebFormApplications/rpg2_spaceAdventure/rpg2_pa/engine/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Player
+{
+    // Properties
+    public string Location { get; set; }
+    public List<string> Codes { get; set; }
+    public float OxygenLevel { get; set; }
+    public int SpaceSuitIntegrity { get; set; }
+
+    // Constructor
+    public Player(string location, List<string> codes, float oxygenLevel, int spaceSuitIntegrity)
+    {
+        Location = location;
+        Codes = codes;
+        OxygenLevel = oxygenLevel;
+        SpaceSuitIntegrity = spaceSuitIntegrity;
+    }
+
+}
---
+++ WebFormApplications/rpg2_spaceAdventure/rpg2_pa/rpg2_pa/rpg_spaceAdventure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rpg2_pa
+{
+    public partial class rpg_spaceAdventure : Form
+    {
+        private Player _player1;
+
+        public rpg_spaceAdventure()
+        {
+            InitializeComponent();
+            _player1 = new Player("start", new List<string> { "code1", "code2", "code3" }, 100, 100);
+        }
+    }
+}
+
+
